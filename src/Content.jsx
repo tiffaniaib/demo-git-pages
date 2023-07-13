98cd09956ffb56ed375ccb55b0f54d128121f47a
[Tiffania] <ielenatiffania@gmail.com>
2023-07-13 10:31:37 -0400

    connect to remote repository

---
+++ src/Content.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div>This is the Content component</div>
+  )
+}
+
+export default Content
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>This is the Footer component</div>
+  )
+}
+
+export default Footer
